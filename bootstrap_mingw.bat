00433c486b4d9bc5c85e21d09433688ce36f424d
Jed Frey <jed-frey@users.noreply.github.com>
2017-11-07 23:15:30 -0500

    Update bootstrap_mingw.bat

--- bootstrap_mingw.bat
+++ bootstrap_mingw.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-: If WinPython URL is not defined (set through Jenkins, etc)
+: If download URL is not defined (set through Jenkins, etc)
 IF "%MINGW_URL%"=="" (
 : Use a known default
 set URL=https://nuwen.net/files/mingw/mingw-15.3.exe
@@ -7,8 +7,7 @@ set URL=https://nuwen.net/files/mingw/mingw-15.3.exe
 : Otherwise use the given URL.
 set URL=%MINGW_URL%
 )
-
-: Generate the package name based on URL. 
+: Set download package name.
 set PKG=mingw.exe
 
 : If the package doesn't exist locally, download it.
@@ -16,5 +15,5 @@ if not exist %PKG% %~dp0curl.exe -L -o %PKG% %URL%
 : If the package has not been 'installed' install it.
 if not exist %~dp0MinGW 7z x %PKG% -y
 
-: Load the MinGW environment so other scripts can call MinGW.
+: Load the MinGW environment so other scripts can call MinGW tools.
 call %~dp0MinGW\set_distro_paths.bat
