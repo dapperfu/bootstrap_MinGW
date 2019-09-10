88d56380ec1b5b4d0970d684afef31995926ed1a
user <user@machine.lan>
2019-09-10 19:46:28 -0400

    2019-Q3 Updates

--- bootstrap_mingw.bat
+++ bootstrap_mingw.bat
@@ -2,19 +2,19 @@
 : If download URL is not defined (set through Jenkins, etc)
 IF "%MINGW_URL%"=="" (
 : Use a known default
-set URL=https://nuwen.net/files/mingw/mingw-16.0.exe
+set URL=https://nuwen.net/files/mingw/mingw-16.1.exe
 ) ELSE (
 : Otherwise use the given URL.
 set URL=%MINGW_URL%
 )
 : Set download package name.
 set PKG=mingw.exe
-
 cd %~dp0
 : If the package doesn't exist locally, download it.
 if not exist %PKG% %~dp0curl.exe -L -o %~dp0%PKG% %URL%
 : If the package has not been 'installed' install it.
 if not exist %~dp0MinGW 7z x %PKG% -y -o%~dp0
 
+: *You see nothing.*
 attrib +r %~dp0MinGW
 attrib +h %~dp0.
--- env_mingw.bat
+++ env_mingw.bat
@@ -1,2 +1,3 @@
 : Load the MinGW environment so other scripts can call MinGW tools.
 call %~dp0MinGW\set_distro_paths.bat
+set PATH=%PATH%;%~dp0MinGW\git\usr\bin
